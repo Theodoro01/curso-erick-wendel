a918870ae727e02bfe98e95ffbca04506f158eaa
Theodoro Vieira <theodorovieira01@Theodoros-MacBook-Air.local>
2024-08-05 18:01:03 -0300

    continua

--- aula04-coverage-e2e/api.js
+++ aula04-coverage-e2e/api.js
@@ -1,10 +1,32 @@
 const http = require('http')
+const DEFAULT_USER  = {username: "ErickWendel", password: "123" }
 
 const routes = {
     '/contact:GET': (request, response) => {
         response.write('contact us page')
         return response.end()
     },
+
+    '/login:POST': async (request, response) => {
+        // response é um iterator
+        for await (const data of request){
+            const user = JSON.parse(data)
+            console.log(user)
+            if (
+                user.username !== DEFAULT_USER.username ||
+                user.password !== DEFAULT_USER.password
+            ) {
+                console.log("opa")
+                response.writeHead(401)
+                response.write("Logging failed!")
+                return response.end()
+            }
+            console.log("opa 2")
+            response.write("Logging has succeeded!")
+            return response.end()
+        }
+    },
+
     default: (request, response) => {
         response.write('Hello World!')
         return response.end()
--- aula04-coverage-e2e/api.test.js
+++ aula04-coverage-e2e/api.test.js
@@ -20,4 +20,21 @@ describe('API Suite test', () => {
             assert.deepStrictEqual(response.text, 'Hello World!')
         })
     })
+    describe('/login', () => {
+        it('should login sucessfully on the login route and return HTTP Status 200', async () => {
+            const response = await request(app)
+                .post('/login')
+                .send({username: "ErickWendel", password: "123"})
+                .expect(200)
+            assert.deepStrictEqual(response.text, 'Logging has succeeded!')
+        })
+        it('should unauthorize a request when requesting it using wrong credentials and return HTTP Status 401', async () => {
+            const response = await request(app)
+                .post('/login')
+                .send({username: "XuxaDaSilva", password: "321"})
+                .expect(401)
+            console.log("response", response.unauthorized)
+            assert.deepStrictEqual(response.text, 'Logging has succeeded!')
+        })
+    })
 })
